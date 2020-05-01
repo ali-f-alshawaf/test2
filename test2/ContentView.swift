cc5dd6aa9827544572b326e5d855d361a4c7b575
ali84sama <ali.f.alshawaf@gmail.com>
2020-05-01 20:10:34 +0300

    Initial Commit

---
+++ test2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test2
+//
+//  Created by ALI on 5/1/20.
+//  Copyright © 2020 ALI. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
